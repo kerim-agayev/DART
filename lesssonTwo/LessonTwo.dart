cb829f0f46f4d385f097bca6e4d4dc35486cf13f
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2024-01-17 14:52:05 +0400

    Create LessonTwo.dart

---
+++ lesssonTwo/LessonTwo.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //!1.
+const name = "Kerim";
+
+
+final surname;
+surname = "Agayev";
+
+print('my name is $name $surname');
+//final and const tek ferqi - final runtimeda deyeri belirlenir, const ise compile zamani, sonradan ikisinin de deyerini deyisdirmek olmur
+
+}
