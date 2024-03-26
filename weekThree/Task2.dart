ff5acbbeea1c7524e97502929eb70c0ae96bcd0c
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2024-03-26 13:15:28 -0700

    Update Task2.dart

--- weekThree/Task2.dart
+++ weekThree/Task2.dart
@@ -8,26 +8,34 @@ void main(List<String> args) {
   // consoleda siz o reqemi tapana qeder print verirsiz, yaxinlasdiz, uzaqlasdiz
   Random random = Random();
   int randomNumber = random.nextInt(101);
+  int chance = 5;
+  print(
+      "yeni oyuna xos geldiniz, reqemi tapib qalib gelmek ucun 5 shansiniz var.");
   print("xahis edirik bir reqem daxil edin");
   while (true) {
     try {
       String input = stdin.readLineSync()!;
       int userNumber = int.parse(input);
-
+      if (chance == 0) {
+        print('meglub oldunuz');
+        break;
+      }
       if (randomNumber == userNumber) {
         print("reqemler beraberdir");
         break;
       } else if (randomNumber - userNumber > 0) {
+        chance--;
         if (randomNumber - userNumber <= 5) {
-          print("reqemi bir az daha boyuk secin");
+          print("reqemi bir az daha boyuk secin, $chance shansiniz qaldi");
         } else {
-          print("reqemi daha boyuk secin");
+          print("reqemi daha boyuk secin, $chance shansiniz qaldi");
         }
       } else if (userNumber - randomNumber > 0) {
+        chance--;
         if (userNumber - randomNumber <= 5) {
-          print("reqemi bir az daha kicik secin");
+          print("reqemi bir az daha kicik secin, $chance shansiniz qaldi");
         } else {
-          print("reqemi  daha kicik secin");
+          print("reqemi  daha kicik secin, $chance shansiniz qaldi");
         }
       }
     } catch (e) {
